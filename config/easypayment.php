042ef1e5bdff1c88a2a09353610b6f9df1db84be
sahidur <sahidurrahman@appinionbd.com>
2022-03-31 18:24:00 +0600

    initial commit

--- config/easypayment.php
+++ config/easypayment.php
@@ -49,10 +49,10 @@ return [
 
 	'gateway' => [
 		'bkash' => [
-			'app_key' => env('BKASH_APP_KEY', ''),
-			'app_secret' => env('BKASH_APP_SECRET', ''),
-			'username' => env('BKASH_APP_SECRET', ''),
-			'password' => env('BKASH_APP_SECRET', ''),
+			'app_key' => env('BKASH_APP_KEY', '5tunt4masn6pv2hnvte1sb5n3j'),
+			'app_secret' => env('BKASH_APP_SECRET', '1vggbqd4hqk9g96o9rrrp2jftvek578v7d2bnerim12a87dbrrka'),
+			'username' => env('BKASH_APP_SECRET', 'sandboxTestUser'),
+			'password' => env('BKASH_APP_SECRET', 'hWD@8vtzw0'),
 		],
 	],
 ];
